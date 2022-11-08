67965e06b223aad58a81d17b39c4d9689e30aa59
Oliv751 <olivierlopez751@gmail.com>
2022-11-08 13:47:38 +0100

    express 03 release

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
@@ -22,7 +24,8 @@ const userHandlers = require("./userHandlers");
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 
-
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
